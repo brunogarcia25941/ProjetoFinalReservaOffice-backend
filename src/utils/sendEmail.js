f3e2fe375b6df081cd8454dc0af32fb5ac42bb03
Bernardo Alves <bernardomiguelalves34@gmail.com>
2026-03-23 23:59:40 +0000

    SenderEmailFix

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -6,14 +6,17 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true, 
+    service: 'gmail', 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-   
+    
+    family: 4, 
     tls: {
         rejectUnauthorized: false
-    }
+    },
+    connectionTimeout: 15000 
 });
 
 
