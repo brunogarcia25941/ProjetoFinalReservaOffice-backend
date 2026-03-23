d5eefc04ec01909920d5e0c915eea13bf46d99bc
Bernardo Alves <bernardomiguelalves34@gmail.com>
2026-03-23 23:46:01 +0000

    fix porta sendEmail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,13 +2,19 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
    
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+   
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 
 
     const mailOptions = {
